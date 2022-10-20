d2ab7478af5d8181ec8aea11591d63a705d0e4e5
User <User@DESKTOP-A50L8A1>
2022-10-20 17:58:25 +1000

    Добавьте файлы проекта.

---
+++ IndependentWork17/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите числовой номер");
+            //int accountNumbersInt=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Введите сумму");
+            //decimal accountBalance=Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("Ведите ФИО");
+            //string name=Console.ReadLine();
+            //BankAccounts<int> bankAccounts1 = new BankAccounts<int>(accountNumbersInt, accountBalance, name);
+            //Console.WriteLine(bankAccounts1.GetInfo());
+
+
+
+            //Console.WriteLine("Введите числовой номер");
+            //string accountNumbersString = Console.ReadLine();
+            //Console.WriteLine("Введите сумму");
+            //accountBalance = Convert.ToDecimal(Console.ReadLine());
+            //Console.WriteLine("Ведите ФИО");
+            //name = Console.ReadLine();
+            //BankAccounts<string> bankAccounts2 = new BankAccounts<string>(accountNumbersString, accountBalance, name);
+            //Console.WriteLine(bankAccounts1.GetInfo());
+
+            BankAccounts<int> bankAccounts1=new BankAccounts<int>();
+            bankAccounts1.Input();
+            Console.WriteLine(bankAccounts1.GetInfo());
+
+            BankAccounts<string> bankAccounts2 = new BankAccounts<string>();
+            bankAccounts2.Input();
+            Console.WriteLine(bankAccounts2.GetInfo());
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ IndependentWork17/bankAccounts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndependentWork17
+{
+    internal class BankAccounts<T>
+    {
+        public T AccountNumbers { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string Name { get; set; }
+
+        //public BankAccounts(T accountNumbers, decimal accountBalance, string name)
+        //{
+        //    AccountNumbers = accountNumbers;
+        //    AccountBalance = accountBalance;
+        //    Name = name;
+        //}
+        public string GetInfo() 
+        {
+            return $"{AccountNumbers} {AccountBalance} {Name}";
+        }
+        public void Input() 
+        {
+            Console.WriteLine("Введите номер");
+            string stringAccountNumbers=Console.ReadLine();
+            AccountNumbers = (T)Convert.ChangeType(stringAccountNumbers, typeof(T));
+            Console.WriteLine("Введите сумму");
+            AccountBalance = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Ведите ФИО");
+            Name = Console.ReadLine();
+        }
+    }
+
+}
